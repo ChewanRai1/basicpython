4e2e983b912731ce47fe0ece3dac76d8cdfc3d83
ChewanRai1 <220154@softwarica.edu.np>
2024-05-24 21:26:43 +0545

    datastructure in tuple

--- datastructure.py
+++ datastructure.py
@@ -9,3 +9,17 @@
 
 #--------------------------------------------------------------------------------------------
 
+# t1 = ("aalu", "pyaj", "dhaniya", "lasun", "kakra")
+# t2= (1,6,2,7,5,5,2,4,5)
+
+# print(t1[0:3]) #start from indexing 0 to 2
+# print(t2[2:4]) #start from indexing 2-3
+
+#--------------------------------------------------------------------------------------------
+
+# packing and unpacking
+
+t =("softwarica", 25, "Chewan")   #packing
+(college, age, name) =t           #unpacking
+
+print(college)
